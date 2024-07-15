48266b83e8daa3db0e7fea24337a830bae21093b
yahyaayyas1 <yahyaayyas112@gmail.com>
2024-07-15 23:29:11 +0700

    hello world

--- src/main/java/com/example/jangkau/controllers/HelloController.java
+++ src/main/java/com/example/jangkau/controllers/HelloController.java
@@ -2,11 +2,14 @@ package com.example.jangkau.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@RequestMapping("/hello")
 public class HelloController {
 
-    @GetMapping("/hello")
+    @GetMapping()
     public String hello() {
         return "hello";
     }
