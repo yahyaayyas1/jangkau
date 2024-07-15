376d54eb8d38bee42c9a786bce2294bf91dc7c97
yahyaayyas1 <yahyaayyas112@gmail.com>
2024-07-15 22:18:39 +0700

    hello world

--- src/main/java/com/example/jangkau/JangkauApplication.java
+++ src/main/java/com/example/jangkau/JangkauApplication.java
@@ -2,6 +2,8 @@ package com.example.jangkau;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class JangkauApplication {
@@ -10,4 +12,14 @@ public class JangkauApplication {
 		SpringApplication.run(JangkauApplication.class, args);
 	}
 
+
+}
+
+@RestController
+class HelloController {
+
+	@GetMapping("/hello")
+	public String hello() {
+		return "Hello, World!";
+	}
 }
