67a56f60c2f42ea7fe2c88081f20bba0e3c48cbc
yahyaayyas1 <yahyaayyas112@gmail.com>
2024-08-19 12:36:03 +0700

    update

--- src/main/java/com/example/jangkau/security/Oauth2ResourceServerConfiguration.java
+++ src/main/java/com/example/jangkau/security/Oauth2ResourceServerConfiguration.java
@@ -53,6 +53,7 @@ public class Oauth2ResourceServerConfiguration extends ResourceServerConfigurerA
                 .antMatchers(HttpMethod.GET, "/bank-accounts/user/{userId}").hasAnyAuthority("ROLE_USER", "ROLE_ADMIN")
                 .antMatchers(HttpMethod.GET, "/saved-accounts").hasAnyAuthority("ROLE_USER", "ROLE_ADMIN")
                 .antMatchers(HttpMethod.GET, "/qris/generate-qr").hasAnyAuthority("ROLE_USER", "ROLE_ADMIN")
+                .antMatchers(HttpMethod.POST, "/qris/generate-qr").hasAnyAuthority("ROLE_USER", "ROLE_ADMIN")
                 .antMatchers(HttpMethod.GET, "/history").hasAnyAuthority("ROLE_USER", "ROLE_ADMIN")
                 .antMatchers(HttpMethod.POST, "/history").hasAnyAuthority("ROLE_USER", "ROLE_ADMIN")
                 .antMatchers(HttpMethod.OPTIONS, "/history").hasAnyAuthority("ROLE_USER", "ROLE_ADMIN")
